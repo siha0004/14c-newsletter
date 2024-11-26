93d4a1992f00bc263d868dacf6b89535feb4e8e6
Sif Melkane <siha0004@stud.kea.dk>
2024-11-26 16:33:46 +0100

    Small styling changes

--- src/app/update/[id]/page.jsx
+++ src/app/update/[id]/page.jsx
@@ -41,7 +41,7 @@ async function page({ params }) {
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start">
         <Link href="/">Go back</Link>
         <form action={update}>
